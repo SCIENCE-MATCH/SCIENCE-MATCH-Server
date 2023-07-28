2cb26b74ca232d00875db65ed154aea686f6c0d2
dong2ast <kdh01132006@naver.com>
2023-07-28 14:20:05 +0900

    fix: 스프링 빈 등록

--- src/main/java/com/sciencematch/sciencematch/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/sciencematch/sciencematch/jwt/JwtAuthenticationEntryPoint.java
@@ -5,12 +5,14 @@ import com.sciencematch.sciencematch.common.dto.ApiResponseDto;
 import com.sciencematch.sciencematch.exception.ErrorStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     private final ObjectMapper mapper = new ObjectMapper();
 
