f7dd0b6ac75869d896088b9314575111c0ef064f
dong2ast <kdh01132006@naver.com>
2023-08-04 21:54:31 +0900

    feat: S3 관련 설정 추가 | 버킷 세팅은 추후 다시 해야함

--- src/main/java/com/sciencematch/sciencematch/exception/ErrorStatus.java
+++ src/main/java/com/sciencematch/sciencematch/exception/ErrorStatus.java
@@ -12,6 +12,7 @@ public enum ErrorStatus {
      */
     VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
     INVALID_TOKEN_INFO_EXCEPTION(HttpStatus.BAD_REQUEST, "토큰 혹은 만료시간 설정이 잘못되었습니다."),
+    INVALID_MULTIPART_EXTENSION_EXCEPTION(HttpStatus.BAD_REQUEST, "허용되지 않은 타입의 파일입니다."),
 
     /**
      * 401 UNAUTHORIZED
@@ -30,6 +31,8 @@ public enum ErrorStatus {
      * 404 NOT FOUND
      */
     NOT_FOUND_USER_EXCEPTION(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다"),
+    NOT_FOUND_IMAGE_EXCEPTION(HttpStatus.NOT_FOUND, "존재하지 않는 이미지입니다."),
+    NOT_FOUND_SAVE_IMAGE_EXCEPTION(HttpStatus.NOT_FOUND, "이미지 저장에 실패했습니다"),
 
     /**
      * 409 CONFLICT
