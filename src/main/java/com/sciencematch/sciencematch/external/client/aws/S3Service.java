312ad6d32138714f048677aec28fa7b33923cc60
dong2ast <kdh01132006@naver.com>
2023-08-07 16:14:33 +0900

    feat: S3 관련 설정 추가 및 Aws 버킷 세팅

--- src/main/java/com/sciencematch/sciencematch/external/client/aws/S3Service.java
+++ src/main/java/com/sciencematch/sciencematch/external/client/aws/S3Service.java
@@ -27,44 +27,44 @@ public class S3Service {
 
     private final AmazonS3 amazonS3;
 
-//    @Value("${cloud.aws.credentials.accessKey}")
-//    private String accessKey;
-//
-//    @Value("${cloud.aws.credentials.secretKey}")
-//    private String secretKey;
-//
-//    @Value("${cloud.aws.s3.bucket}")
-//    private String bucket;
-//
-//    @Value("${cloud.aws.region.static}")
-//    private String region;
-//
-//    @PostConstruct
-//    public AmazonS3Client amazonS3Client() {
-//        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-//        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-//            .withRegion(region)
-//            .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-//            .build();
-//    }
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
 
-//    public String uploadImage(MultipartFile multipartFile, String folder) {
-//        String fileName = createFileName(multipartFile.getOriginalFilename());
-//        //inputStream 통해 Byte로 파일이 전달되기 때문에 파일에 대한 정보가 없어서 objectMetadata 같이 넘겨야 함
-//        ObjectMetadata objectMetadata = new ObjectMetadata();
-//        objectMetadata.setContentLength(multipartFile.getSize()); //TODO: 파일 크기 제한
-//        objectMetadata.setContentType(multipartFile.getContentType());
-//        try (InputStream inputStream = multipartFile.getInputStream()) { //Statement Class의 인스턴스나 Stream 타입의 클래스들이 동작 후 필요로하는 close() 메소드를 자동실행해주는 공간
-//            amazonS3.putObject(
-//                new PutObjectRequest(bucket + "/" + folder + "/image", fileName, inputStream,
-//                    objectMetadata)
-//                    .withCannedAcl(CannedAccessControlList.PublicRead));
-//            return amazonS3.getUrl(bucket + "/" + folder + "/image", fileName).toString();
-//        } catch (IOException e) {
-//            throw new NotFoundException(ErrorStatus.NOT_FOUND_SAVE_IMAGE_EXCEPTION,
-//                ErrorStatus.NOT_FOUND_SAVE_IMAGE_EXCEPTION.getMessage());
-//        }
-//    }
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @PostConstruct
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+            .withRegion(region)
+            .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+            .build();
+    }
+
+    public String uploadImage(MultipartFile multipartFile, String folder) {
+        String fileName = createFileName(multipartFile.getOriginalFilename());
+        //inputStream 통해 Byte로 파일이 전달되기 때문에 파일에 대한 정보가 없어서 objectMetadata 같이 넘겨야 함
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(multipartFile.getSize()); //TODO: 파일 크기 제한
+        objectMetadata.setContentType(multipartFile.getContentType());
+        try (InputStream inputStream = multipartFile.getInputStream()) { //Statement Class의 인스턴스나 Stream 타입의 클래스들이 동작 후 필요로하는 close() 메소드를 자동실행해주는 공간
+            amazonS3.putObject(
+                new PutObjectRequest(bucket + "/" + folder + "/image", fileName, inputStream,
+                    objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+            return amazonS3.getUrl(bucket + "/" + folder + "/image", fileName).toString();
+        } catch (IOException e) {
+            throw new NotFoundException(ErrorStatus.NOT_FOUND_SAVE_IMAGE_EXCEPTION,
+                ErrorStatus.NOT_FOUND_SAVE_IMAGE_EXCEPTION.getMessage());
+        }
+    }
 
     // 파일명 (중복 방지)
     private String createFileName(String fileName) {
