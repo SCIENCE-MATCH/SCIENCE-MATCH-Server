bdf5f2006b274c25594a84bde1fe02bedec275d7
dong2ast <kdh01132006@naver.com>
2023-08-17 13:57:08 +0900

    feat: 더미데이터 추가

--- src/main/java/com/sciencematch/sciencematch/InitDb.java
+++ src/main/java/com/sciencematch/sciencematch/InitDb.java
@@ -1,6 +1,10 @@
 package com.sciencematch.sciencematch;
 
+import com.sciencematch.sciencematch.domain.Student;
+import com.sciencematch.sciencematch.domain.Teacher;
+import com.sciencematch.sciencematch.domain.enumerate.Authority;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,10 +26,27 @@ public class InitDb {
     @RequiredArgsConstructor
     static class InitService {
         private final EntityManager em;
+        private final PasswordEncoder passwordEncoder;
 
         @Transactional
         public void dbInit() {
+            Teacher teacher = Teacher.builder()
+                .email("science@gmail.com")
+                .name("김사매")
+                .password(passwordEncoder.encode("test1234"))
+                .phoneNum("01012345678")
+                .authority(Authority.ROLE_TEACHER)
+                .build();
+            em.persist(teacher);
 
+            Student student = Student.builder()
+                .grade("고1")
+                .name("김사피")
+                .parentNum("01013467946")
+                .phoneNum("01087654321")
+                .authority(Authority.ROLE_STUDENT)
+                .build();
+            em.persist(student);
         }
     }
 }
