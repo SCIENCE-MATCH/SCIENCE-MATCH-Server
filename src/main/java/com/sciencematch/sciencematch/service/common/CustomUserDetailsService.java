742b890237ff7d433cbb4aa5fb0526cf9ba06002
dong2ast <kdh01132006@naver.com>
2023-08-21 10:40:38 +0900

    add: 로그인시에 Admin인지 아닌지 구분하는 로직 추가

--- src/main/java/com/sciencematch/sciencematch/service/common/CustomUserDetailsService.java
+++ src/main/java/com/sciencematch/sciencematch/service/common/CustomUserDetailsService.java
@@ -1,8 +1,11 @@
 package com.sciencematch.sciencematch.service.common;
 
+import com.sciencematch.sciencematch.domain.Admin;
 import com.sciencematch.sciencematch.domain.Teacher;
+import com.sciencematch.sciencematch.infrastructure.AdminRepository;
 import com.sciencematch.sciencematch.infrastructure.TeacherRepository;
 import java.util.Collections;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -16,12 +19,24 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
     private final TeacherRepository teacherRepository;
+    private final AdminRepository adminRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return teacherRepository.findByEmail(username)
-            .map(this::createUserDetails)
-            .orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
+        if (!Objects.equals(username, "hyh12100863@gmail.com")){
+            return teacherRepository.findByEmail(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
+        } else {
+            Admin admin = adminRepository.getAdminById(1L);
+            GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(
+                admin.getAuthority().toString());
+            return new User(
+                admin.getEmail(),
+                admin.getPassword(),
+                Collections.singleton(grantedAuthority)
+            );
+        }
     }
 
     // DB에 User 값이 존재한다면 UserDetails 객체로 만들어서 리턴
