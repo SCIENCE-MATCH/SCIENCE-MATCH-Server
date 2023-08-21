6d9b3b999dfe213c0ad2cb78652da023bac3ce0d
dong2ast <kdh01132006@naver.com>
2023-08-21 10:09:02 +0900

    feat: Auditing 엔티티 추가

--- src/main/java/com/sciencematch/sciencematch/ScienceMatchApplication.java
+++ src/main/java/com/sciencematch/sciencematch/ScienceMatchApplication.java
@@ -2,8 +2,10 @@ package com.sciencematch.sciencematch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ScienceMatchApplication {
 
 	public static void main(String[] args) {
