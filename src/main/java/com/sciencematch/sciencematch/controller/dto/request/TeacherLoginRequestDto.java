54b2dd7b41daf616184c2e45e5e0c15897b90555
dong2ast <kdh01132006@naver.com>
2023-09-08 13:54:40 +0900

    chore: 스웨거 기본 로그인 아이디를 더미 선생님 아이디로 변경

--- src/main/java/com/sciencematch/sciencematch/controller/dto/request/TeacherLoginRequestDto.java
+++ src/main/java/com/sciencematch/sciencematch/controller/dto/request/TeacherLoginRequestDto.java
@@ -16,8 +16,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 public class TeacherLoginRequestDto {
     @Email(message = "이메일 형식이 맞지 않습니다.")
     @NotBlank
-//    @Schema(description = "유저 아이디(이메일 주소)", example = "science@gmail.com")
-    @Schema(description = "유저 아이디(이메일 주소)", example = "hyh12100863@gmail.com")
+    @Schema(description = "유저 아이디(이메일 주소)", example = "science@gmail.com")
+//    @Schema(description = "유저 아이디(이메일 주소)", example = "hyh12100863@gmail.com")
     private String email;
 
     @NotNull
@@ -25,8 +25,8 @@ public class TeacherLoginRequestDto {
             regexp="(?=.*[0-9])(?=.*[a-zA-Z]).{8,16}",
             message = "비밀번호는 영문과 숫자가 포함된 8자 ~ 16자의 비밀번호여야 합니다"
     )
-    @Schema(description = "비밀번호", example = "hyh12100863@")
-//    @Schema(description = "비밀번호", example = "test1234")
+//    @Schema(description = "비밀번호", example = "hyh12100863@")
+    @Schema(description = "비밀번호", example = "test1234")
     private String password;
 
     public UsernamePasswordAuthenticationToken toAuthentication() {
