eb4d846bbf7859f18a357558773eecde6725a241
dong2ast <kdh01132006@naver.com>
2023-09-25 11:14:42 +0900

    feat: validation 예외 advice 메서드 구현

--- src/main/java/com/sciencematch/sciencematch/common/dto/ApiResponseDto.java
+++ src/main/java/com/sciencematch/sciencematch/common/dto/ApiResponseDto.java
@@ -14,7 +14,7 @@ public class ApiResponseDto<T> {
     private final String message;
     private T data;
 
-    public static ApiResponseDto success(SuccessStatus successStatus) {
+    public static ApiResponseDto<?> success(SuccessStatus successStatus) {
         return new ApiResponseDto<>(successStatus.getHttpStatus().value(), successStatus.getMessage());
     }
 
@@ -22,11 +22,15 @@ public class ApiResponseDto<T> {
         return new ApiResponseDto<T>(successStatus.getHttpStatus().value(), successStatus.getMessage(), data);
     }
 
-    public static ApiResponseDto error(ErrorStatus errorStatus) {
+    public static ApiResponseDto<?> error(ErrorStatus errorStatus) {
         return new ApiResponseDto<>(errorStatus.getHttpStatus().value(), errorStatus.getMessage());
     }
 
-    public static ApiResponseDto error(ErrorStatus errorStatus, String message) {
+    public static ApiResponseDto<?> error(ErrorStatus errorStatus, String message) {
         return new ApiResponseDto<>(errorStatus.getHttpStatusCode(), message);
+    }
+
+    public static ApiResponseDto<?> error(Integer errorCode, String message) {
+        return new ApiResponseDto<>(errorCode, message);
     }
 }
