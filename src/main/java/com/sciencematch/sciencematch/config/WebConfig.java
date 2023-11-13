b44964775efab1a43533bd05e8977d38223a6f56
dong2ast <kdh01132006@naver.com>
2023-11-13 17:39:02 +0900

    :bug: fix: header 수정

--- src/main/java/com/sciencematch/sciencematch/config/WebConfig.java
+++ src/main/java/com/sciencematch/sciencematch/config/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/auth/**")
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .allowedHeaders("Authorization", "Content-Type", "Refresh")
                 .exposedHeaders(HttpHeaders.LOCATION);
     }
 }
