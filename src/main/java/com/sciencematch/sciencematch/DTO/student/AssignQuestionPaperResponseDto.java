317d7326ec7cce913207a3867f82bccbca2f5f66
dong2ast <kdh01132006@naver.com>
2024-02-08 09:24:08 +0900

    modify: 학습지 조회시 맞은 개수도 같이 표시되게 변경

--- src/main/java/com/sciencematch/sciencematch/DTO/student/AssignQuestionPaperResponseDto.java
+++ src/main/java/com/sciencematch/sciencematch/DTO/student/AssignQuestionPaperResponseDto.java
@@ -2,6 +2,7 @@ package com.sciencematch.sciencematch.DTO.student;
 
 import com.sciencematch.sciencematch.Enums.AssignStatus;
 import com.sciencematch.sciencematch.Enums.Subject;
+import com.sciencematch.sciencematch.domain.question.Answer;
 import com.sciencematch.sciencematch.domain.question.AssignQuestions;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,12 +16,14 @@ public class AssignQuestionPaperResponseDto {
     private AssignStatus assignStatus;
     private String title;
     private Integer questionNum;
+    private Long correctNum;
 
     public static AssignQuestionPaperResponseDto of(AssignQuestions assignQuestions) {
         return new AssignQuestionPaperResponseDto(assignQuestions.getId(),
             assignQuestions.getSubject(), assignQuestions.getAssignStatus(),
             assignQuestions.getQuestionPaper().getTitle(),
-            assignQuestions.getQuestionPaper().getQuestionNum());
+            assignQuestions.getQuestionPaper().getQuestionNum(),
+            assignQuestions.getAnswer().stream().filter(Answer::getRightAnswer).count());
     }
 
 
