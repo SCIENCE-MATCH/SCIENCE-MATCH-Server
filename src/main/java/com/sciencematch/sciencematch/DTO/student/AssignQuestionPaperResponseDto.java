b61ad84e6abfd2d8733545696f1c902542798c41
dong2ast <kdh01132006@naver.com>
2024-02-24 20:30:55 +0900

    modify: 학습지 조회시 점수/총점 보여주게 변경

--- src/main/java/com/sciencematch/sciencematch/DTO/student/AssignQuestionPaperResponseDto.java
+++ src/main/java/com/sciencematch/sciencematch/DTO/student/AssignQuestionPaperResponseDto.java
@@ -2,7 +2,6 @@ package com.sciencematch.sciencematch.DTO.student;
 
 import com.sciencematch.sciencematch.Enums.AssignStatus;
 import com.sciencematch.sciencematch.Enums.Subject;
-import com.sciencematch.sciencematch.domain.question.Answer;
 import com.sciencematch.sciencematch.domain.question.AssignQuestions;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,15 +14,15 @@ public class AssignQuestionPaperResponseDto {
     private Subject subject;
     private AssignStatus assignStatus;
     private String title;
-    private Integer questionNum;
-    private Long correctNum;
+    private Integer score;
+    private Integer totalScore;
 
     public static AssignQuestionPaperResponseDto of(AssignQuestions assignQuestions) {
         return new AssignQuestionPaperResponseDto(assignQuestions.getId(),
             assignQuestions.getSubject(), assignQuestions.getAssignStatus(),
             assignQuestions.getQuestionPaper().getTitle(),
-            assignQuestions.getQuestionPaper().getQuestionNum(),
-            assignQuestions.getAnswer().stream().filter(Answer::getRightAnswer).count());
+            assignQuestions.getScore(),
+            assignQuestions.getTotalScore());
     }
 
 
