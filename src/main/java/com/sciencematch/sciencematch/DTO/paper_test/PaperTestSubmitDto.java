758ed9b2a51c71ef01cb4b520fe1c97a3a326fb1
dong2ast <kdh01132006@naver.com>
2024-06-24 09:43:08 +0900

    [FIX] 1:1 단일 질문 출제 기능 수정

--- src/main/java/com/sciencematch/sciencematch/DTO/paper_test/PaperTestSubmitDto.java
+++ src/main/java/com/sciencematch/sciencematch/DTO/paper_test/PaperTestSubmitDto.java
@@ -2,10 +2,14 @@ package com.sciencematch.sciencematch.DTO.paper_test;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaperTestSubmitDto {
 
     @Schema(example = "52")
