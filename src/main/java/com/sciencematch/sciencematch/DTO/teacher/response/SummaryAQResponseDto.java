f3adc52e27d0a7e4436a233635a4b9e76ebb4564
dong2ast <kdh01132006@naver.com>
2024-07-02 22:09:23 +0900

    [FIX] 학습 내역 조회 오류 수정

--- src/main/java/com/sciencematch/sciencematch/DTO/teacher/response/SummaryAQResponseDto.java
+++ src/main/java/com/sciencematch/sciencematch/DTO/teacher/response/SummaryAQResponseDto.java
@@ -21,7 +21,7 @@ public class SummaryAQResponseDto {
 
 
     public static SummaryAQResponseDto of(AssignQuestions assignQuestions) {
-        Integer correctPercent = assignQuestions.getTotalScore() == 0 ? 0 : (int) ((double) assignQuestions.getScore() / assignQuestions.getTotalScore()) * 100;
+        Integer correctPercent = assignQuestions.getTotalScore() == 0 ? 0 : (int) (((double) assignQuestions.getScore() / assignQuestions.getTotalScore()) * 100);
         return new SummaryAQResponseDto(assignQuestions.getId(), assignQuestions.getQuestionPaper().getCategory(),
             assignQuestions.getQuestionPaper().getTitle(), assignQuestions.getSubject(),
             assignQuestions.getQuestionNum(), correctPercent, assignQuestions.getAssignStatus());
