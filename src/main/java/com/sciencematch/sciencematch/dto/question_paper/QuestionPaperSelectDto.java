dd0e43e4a1a290c7cdab25d7aa85cd73584f4da9
stampHo <inho8024s@gmail.com>
2025-04-23 16:48:02 +0900

    [ROLLBACK] 수정 사항 취소소

--- src/main/java/com/sciencematch/sciencematch/dto/question_paper/QuestionPaperSelectDto.java
+++ src/main/java/com/sciencematch/sciencematch/dto/question_paper/QuestionPaperSelectDto.java
@@ -1,7 +1,7 @@
 package com.sciencematch.sciencematch.dto.question_paper;
 
-// import com.sciencematch.sciencematch.enums.QuestionTag;
-// import com.sciencematch.sciencematch.enums.School;
+import com.sciencematch.sciencematch.enums.QuestionTag;
+import com.sciencematch.sciencematch.enums.School;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.Data;
@@ -10,10 +10,10 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Data
 public class QuestionPaperSelectDto {
 
-    // @Schema(example = "HIGH")
-    // private School school;
-    // @Schema(example = "NORMAL")
-    // private QuestionTag questionTag;
+    @Schema(example = "HIGH")
+    private School school;
+    @Schema(example = "NORMAL")
+    private QuestionTag questionTag;
 
     @Schema(example = "2023-09-08T05:55:58", type = "string")
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
